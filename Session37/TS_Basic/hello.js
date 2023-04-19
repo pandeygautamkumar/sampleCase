0b504a58da3f06f56933618895a252d86569a0e5
unknown <gautampandeykumargmail.com>
2023-04-19 09:41:28 +0530

    'upload_file'

---
+++ Session37/TS_Basic/hello.js
@@ -0,0 +1,6 @@
+var timeOutMilliSeconds = 3000;
+setTimeout(function () {
+    console.log("Welcome to typescript");
+}, timeOutMilliSeconds);
+console.log("Valid js is valid ts");
+console.log("End of Program");
---
+++ Session37/TS_Basic/hello.ts
@@ -0,0 +1,8 @@
+const timeOutMilliSeconds: number = 3000
+
+setTimeout(() => {
+    console.log("Welcome to typescript")
+}, timeOutMilliSeconds)
+
+console.log("Valid js is valid ts")
+console.log("End of Program")
